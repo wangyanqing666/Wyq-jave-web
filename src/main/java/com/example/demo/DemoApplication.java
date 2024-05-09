275315b3dc5742b9e11c63ad66537a70a6175ef5
yanqing.wang <615970781@qq.com>
2024-05-09 21:38:01 +0800

    完整跑起来的代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
@@ -16,15 +17,19 @@ public class DemoApplication {
      * 信息可以从数据库中取，这边作为demo，暂时写死
      * @return
      */
-    @RequestMapping("home")
-    String home(HttpServletRequest request) {
-        String userName = "Jef";
-        if( userName=="Jef" ){
-            System.out.print("这是 if 语句1");
-        }else if ( userName=="Jef" ){
-            System.out.print("这是 else 语句2");
+    @RequestMapping("home/{userName}")
+    String home(HttpServletRequest request, @PathVariable String userName) {
+        if( "wyq".equals(userName) ){
+            System.out.print("这是 if 语句2225555");
+        }else if ( "lsj".equals(userName) ) {
+            System.out.print("这是 else 语句2244444");
+
+        } else if ( "jsh".equals(userName) ){
+                System.out.print("新增346e4");
+        } else if ( "zhm".equals(userName) ){
+            System.out.print("新增4");
         }else{
-            System.out.print("这是 else 语句3");
+            System.out.print("这是 else 语句334");
 
         }
 
@@ -33,11 +38,21 @@ public class DemoApplication {
         return "欢迎您" + userName + "，这是您的第 " + count + " 次登录";
     }
 
-    @RequestMapping("zhuce")
-    String kaifei(HttpServletRequest request) {
-        String userName = "Jef";
+    @RequestMapping("page/{num}")
+    String kaifei(HttpServletRequest request,@PathVariable Integer num) {
+//        String userName = "Jef";
+        if( num == 1 ){
+            System.out.print("这是 if dfhdughdsiaaaaaughdaiuhfuadih 语句1");
+        }else if (num == 2 ){
+            System.out.print("这是 else 语句2");
+        }else{
+            System.out.print("这是 else 语句3");
+
+        }
+
+
         int count = 100;
-        return "我草";
+        return "page num:="+num;
     }
 
     public static void main(String[] args) {
