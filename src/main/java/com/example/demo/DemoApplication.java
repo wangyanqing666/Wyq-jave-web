811a914d772c0a93e78ac64ad7c20fd6be328be8
yanqing.wang <615970781@qq.com>
2024-05-21 22:07:14 +0800

    123

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
@@ -16,30 +17,27 @@ public class DemoApplication {
      * 信息可以从数据库中取，这边作为demo，暂时写死
      * @return
      */
-    @RequestMapping("home")
-    String home(HttpServletRequest request) {
-        String userName = "Jef";
-        if( userName=="Jef" ){
-            System.out.print("这是 if 语句1");
-        }else if ( userName=="Jef" ){
-            System.out.print("这是 else 语句2");
-        }else if ( userName=="wyqtest" ){
-            System.out.print("这是wyqtest");
+    @RequestMapping("home/{userName}")
+    String home(HttpServletRequest request, @PathVariable String userName) {
+        if( "wyq".equals(userName) ){
+            System.out.print("这是 if 语句2225555");
+        }else if ( "lsj".equals(userName) ) {
+            System.out.print("这是 else 语句2244444");
+
+        } else if ( "jsh".equals(userName) ){
+            System.out.print("新增346e4");
+        } else if ( "zhm".equals(userName) ){
+            System.out.print("新增4");
+        }else if ( "hej".equals(userName) ){
+            System.out.print("我市新增的hej");
         }else{
-            System.out.print("这是 else 语句3");
+            System.out.print("这是 else 语句334");
 
         }
 
 
         int count = 100;
         return "欢迎您" + userName + "，这是您的第 " + count + " 次登录";
-    }
-
-    @RequestMapping("zhuce")
-    String kaifei(HttpServletRequest request) {
-        String userName = "Jef";
-        int count = 100;
-        return "我草";
     }
 
     public static void main(String[] args) {
